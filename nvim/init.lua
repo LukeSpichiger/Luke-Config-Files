0d54392c4fdc38b11ea6a1d9395ccbecefed1852
LukeSpichiger <155106985+LukeSpichiger@users.noreply.github.com>
2024-02-16 10:19:07 +0100

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+-- Nvim Config Work
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.termguicolors = true
+vim.cmd [[colorscheme habamax]]
